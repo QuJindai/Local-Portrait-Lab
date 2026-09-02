9a8af5a208318db893f370a2d21b1ad66742bbf6
QuJindai <129613167+QuJindai@users.noreply.github.com>
2026-09-02 09:04:12 +0800

    refactor: expose QNN SDXL model layout contract

---
+++ overlay/lib/portrait_lab/infrastructure/qnn_model_layout.dart
@@ -0,0 +1,12 @@
+const qnnSdxlRequiredFiles = <String>[
+  'tokenizer.json',
+  'clip.mnn',
+  'pos_emb.bin',
+  'token_emb.bin',
+  'clip_2.mnn',
+  'pos_emb_2.bin',
+  'token_emb_2.bin',
+  'unet.bin',
+  'vae_encoder.bin',
+  'vae_decoder.bin',
+];
